76f6e76e1b30756c6fd6c13af5d84cdf24725191
sammy-258 <ogunnaikesammy258@gmail.com>
2022-12-01 02:24:12 -0800

    work

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>uncle</title>
-    <link rel="stylesheet" href="bootstrap-5.1.3-dist\css\bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="css/index.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -660,7 +660,7 @@
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
-<script src="bootstrap-5.1.3-dist\js\bootstrap.js"></script>
+<script src="bootstrap.js"></script>
 <script src="https://kit.fontawesome.com/91c295e6a7.js " crossorigin="anonymous "></script>
 <script>
     var swiper = new Swiper(".mySwiper", {
